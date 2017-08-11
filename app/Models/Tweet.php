fa63f65839294725d17c0a0c4d8dd4797a9aaa63
toshiya <1048-jonan@ezweb.ne.jp>
2017-08-11 15:12:08 +0900

    ツイート変更のコミット。

---
+++ app/Models/Tweet.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tweet extends Model
+{
+
+
+    protected $fillable = [
+        'body',
+        'follower_id',
+        ];
+}
+
---
+++ database/migrations/2017_08_09_070341_create_tweets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTweetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('body');
+            $table->timestamps();
+            $table->integer('user_id');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tweets');
+
+    }
+}
